7778f3377300952ce478f7596714243f46c5b9d4
Arun12Muralidharan <arunmurali12@gmail.com>
2023-07-18 18:13:42 +0530

    updated buttons as new components

--- src/App.js
+++ src/App.js
@@ -22,9 +22,12 @@ export default function App() {
 
   return (
     <>
-      <button className="close" onClick={handleClose}>
+      {/* <button className="close" onClick={handleClose}>
         &times;
-      </button>
+      </button> */}
+      <Button className={"close"} onClick={handleClose}>
+        &times;
+      </Button>
       {isVisible && (
         <div className="steps">
           <div className="numbers">
@@ -32,25 +35,62 @@ export default function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          {/* <p className="message">
+            <h3>Step {step}: </h3>
+            {messages[step - 1]}
+          </p> */}
+          <Message className={"message"}>
+            <>
+              <h3>Step {step}: </h3>
+              {messages[step - 1]}
+            </>
+          </Message>
           <div className="buttons">
-            <button
+            {/* <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
               Previous
-            </button>
-            <button
+            </button> */}
+            <Button
+              backgroundColor={"#7950f2"}
+              color={"#fff"}
+              onClick={handlePrevious}
+            >
+              &#60; &nbsp; Previous
+            </Button>
+            <Button
+              backgroundColor={"#7950f2"}
+              color={"#fff"}
+              onClick={handleNext}
+            >
+              Next &nbsp; &#62;
+            </Button>
+            {/* <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
             >
               Next
-            </button>
+            </button> */}
           </div>
         </div>
       )}
     </>
   );
+}
+
+function Button({ backgroundColor, color, onClick, children, className }) {
+  return (
+    <button
+      style={{ backgroundColor: backgroundColor, color: color }}
+      className={className}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function Message({ children, className }) {
+  return <p className={className}>{children}</p>;
 }
